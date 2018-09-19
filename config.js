00e0abb28ef6eb32bea506140aaa31e7cbb22c32
aueiei <chotipat@siamsquared.com>
2018-09-19 19:52:55 +0700

    Add some config.

--- config.js
+++ config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  contract: {
+    address: '0xd598AC2393Ea26a9b1AA391ba8c4b55F77C278D0',
+    provider: 'wss://kovan.infura.io/_ws',
+  },
+  zxExchangeContractKovan: {
+    address: '0x90fe2af704b34e0224bf2299c838e04d4dcf1364',
+    provider: 'wss://kovan.infura.io/_ws',
+  },
   mnemonic: process.env.MNEMONIC || '', //Your mnemonic
   relayBaseURL: 'https://api.kovan.radarrelay.com/0x',
 };
