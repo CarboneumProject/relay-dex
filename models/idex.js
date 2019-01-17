e13495e91efbd40b249e8e1fc4b668e35f11df2a
aueiei <chotipat@siamsquared.com>
2019-01-17 17:58:55 +0700

    Fix intrinsic gas too low.

--- models/idex.js
+++ models/idex.js
@@ -51,7 +51,7 @@ idex.depositEth = async function depositEth(provider, wei) {
     return await idexContractSign.methods.deposit().send({
       from: provider.addresses[0],
       value: wei,
-      gasLimit: 21000,
+      gasLimit: 1000000,
       gasPrice: web3Sign.eth.gasPrice
     });
   }
