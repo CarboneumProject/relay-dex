0df10c029110e3a17fa266a6af42980bcc918ff9
aueiei <chotipat@siamsquared.com>
2019-01-22 15:22:36 +0700

    Set expire 24 hours, if transaction is pending.

--- models/useRedis.js
+++ models/useRedis.js
@@ -4,6 +4,7 @@ let redis = require("redis"), client = redis.createClient();
 
 useRedis.saveHash = function saveHash(txHash, walletAddress) {
   client.hset('txHash:' + txHash, walletAddress.toLowerCase(), 0);
+  client.expire('txHash:' + txHash, 60 * 60 * 24);  //Expire in 24 hrs.
 };
 
 useRedis.markDeposited = function markDeposited(txHash, walletAddress) {
