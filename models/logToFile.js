cad5e162a92fa4c948ddd55024a3038adba15c36
aueiei <chotipat@siamsquared.com>
2019-01-22 17:45:04 +0700

    Use winston logger.

--- models/logToFile.js
+++ models/logToFile.js
@@ -1,9 +1,20 @@
 const logToFile = {};
-const fs = require('fs');
 
+const {transports, createLogger, format} = require('winston');
 
-logToFile.writeLog = function writeLog(filename, data) {
-  fs.appendFileSync(filename, '[' + new Date().toISOString() + ']: ' + data + '\n');
+logToFile.writeLog = function writeLog(data) {
+  const logger = createLogger({
+    format: format.combine(
+      format.timestamp(),
+      format.json()
+    ),
+    transports: [
+      new transports.Console(),
+      new transports.File({filename: '../logs/error.log', level: 'error'}),
+      new transports.File({filename: '../logs/activity.log', level: 'info'})
+    ]
+  });
+  logger.info(data);
 };
 
 module.exports = logToFile;
