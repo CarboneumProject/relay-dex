043c16ff415e3e591ed8b99862b1f8122c3a67b3
aueiei <chotipat@siamsquared.com>
2019-02-11 17:43:16 +0700

    Remove exponent.

--- loopDepositToIDEX.js
+++ loopDepositToIDEX.js
@@ -12,6 +12,24 @@ const network = config.getNetwork();
 const RESERVED_ETH = '2100000000000000';
 const redis = require("redis");
 
+Number.prototype.noExponents = function () {
+  var data = String(this).split(/[eE]/);
+  if (data.length === 1) return data[0];
+
+  var z = '', sign = this < 0 ? '-' : '',
+    str = data[0].replace('.', ''),
+    mag = Number(data[1]) + 1;
+
+  if (mag < 0) {
+    z = sign + '0.';
+    while (mag++) z += '0';
+    return z + str.replace(/^\-/, '');
+  }
+  mag -= str.length;
+  while (mag--) z += '0';
+  return str + z;
+};
+
 function watchDepositedToLinkWallet() {
   let client = redis.createClient();
   client.keys("txHash:new:*", function (err, txHash_dict) {
@@ -33,14 +51,14 @@ function watchDepositedToLinkWallet() {
                 logToFile.writeLog('loopDeposit', txHash + ' ' + walletAddress + ' have been deposited.');
               } else {
                 useRedis.getAmount(txHash, walletAddress.toLowerCase()).then((amount) => {
-                  let amountDeposited = wei - RESERVED_ETH;
-                  if (amount && amount !== '0') {
-                    amountDeposited = amount;
-                  }
                   useRedis.markDeposited(txHash, walletAddress);
                   console.log(txHash, ': is depositing.');
                   const mappedAddressProvider = relayWallet.getUserWalletProvider(walletAddress);
                   if (tokenAddress === '0x0000000000000000000000000000000000000000') {
+                    let amountDeposited = Number(Number(wei - RESERVED_ETH).toFixed(0)).noExponents();
+                    if (amount && amount !== '0') {
+                      amountDeposited = amount;
+                    }
 
                     idex.depositEth(mappedAddressProvider, amountDeposited).then((respond) => {
                       if (typeof respond === 'object') {
