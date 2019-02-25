e6de201b566331ddedca1251b7188d81fab4fa0f
Artiya Thinkumpang <artiya4u@gmail.com>
2019-02-25 12:09:52 +0700

    Fix var not define.

--- models/mysql.js
+++ models/mysql.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const util = require('util');
 
-pool = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER ||'root',
   password: process.env.DB_PASSWORD ||'password',
