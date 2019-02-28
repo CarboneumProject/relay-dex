e35d13aefb4a5be05b96096c842e41577d745738
Artiya Thinkumpang <artiya4u@gmail.com>
2019-02-28 18:01:47 +0700

    Change config for RPC

--- models/relayWallet.js
+++ models/relayWallet.js
@@ -20,7 +20,7 @@ relayWallet.getUserWalletProvider = function getUserWalletProvider(userAddress) 
 
   return new HDWalletProvider(
     process.env.MNEMONIC || config.mnemonic,
-    rocess.env.RPC_URL || `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    process.env.RPC_URL || `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`,
     accountIndex,
     1
   );
