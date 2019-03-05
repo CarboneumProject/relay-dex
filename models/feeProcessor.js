6cb9ef543e724d301e3007b657569ebefb392af2
aueiei <chotipat@siamsquared.com>
2019-03-06 02:11:41 +0700

    if start with sell token.

--- models/feeProcessor.js
+++ models/feeProcessor.js
@@ -11,53 +11,74 @@ feeProcessor.percentageFee = async function (openTrades, copyOrder, closeTrade, 
   let processedFees = [];
   let updateAmounts = [];
 
-  for (let i = 0; i < openTrades.length && sub_amountLeft > 0; i++) {
-    let openOrder = openTrades[i];
-    let lastAmount = new BigNumber(openOrder.amount_left);
-    sub_amountLeft = sub_amountLeft.sub(lastAmount);
-    let avg = new BigNumber(openOrder.amount_taker).div(openOrder.amount_maker);
+  // If openTrades.length === 0 (Start with short-sales token)
+  let openedPosition = openTrades.length;
+  if (openedPosition > 0) {
+    for (let i = 0; i < openedPosition && sub_amountLeft > 0; i++) {
+      let openOrder = openTrades[i];
+      let lastAmount = new BigNumber(openOrder.amount_left);
+      sub_amountLeft = sub_amountLeft.sub(lastAmount);
+      let avg = new BigNumber(openOrder.amount_taker).div(openOrder.amount_maker);
 
-    let profit = new BigNumber(0);
-    if (sub_amountLeft >= 0) {
-      updateAmounts.push({ 'amountLeft': '0', 'orderId': openOrder.id });
-      profit = (tokenSellLastPrice.sub(avg)).mul(PROFIT_PERCENTAGE).mul(lastAmount);
-    } else {
-      updateAmounts.push({ 'amountLeft': sub_amountLeft.abs().toFixed(0), 'orderId': openOrder.id });
-      profit = (tokenSellLastPrice.sub(avg)).mul(PROFIT_PERCENTAGE).mul(lastAmount.add(sub_amountLeft));
+      let profit = new BigNumber(0);
+      if (sub_amountLeft >= 0) {
+        updateAmounts.push({'amountLeft': '0', 'orderId': openOrder.id});
+        profit = (tokenSellLastPrice.sub(avg)).mul(PROFIT_PERCENTAGE).mul(lastAmount);
+      } else {
+        updateAmounts.push({'amountLeft': sub_amountLeft.abs().toFixed(0), 'orderId': openOrder.id});
+        profit = (tokenSellLastPrice.sub(avg)).mul(PROFIT_PERCENTAGE).mul(lastAmount.add(sub_amountLeft));
+      }
+
+      if (avg < tokenSellLastPrice) {
+        let reward = profit.div(c8LastPrice).mul(network.LEADER_REWARD_PERCENT).toFixed(0);
+        let fee = profit.div(c8LastPrice).mul(network.SYSTEM_FEE_PERCENT).toFixed(0);
+        let C8FEE = profit.div(c8LastPrice.mul(10 ** c8Decimals));
+        sumC8FEE = sumC8FEE.add(C8FEE);
+
+        processedFees.push({
+          'C8FEE': C8FEE,
+          'leader': copyOrder.leader,
+          'follower': copyOrder.follower,
+          'reward': reward,
+          'relayFee': fee,
+          'orderHashes': [openOrder.leader_tx_hash,
+            copyOrder.leader_tx_hash,
+            openOrder.tx_hash,
+            closeTrade.txHash],
+        });
+      } else {
+        processedFees.push({
+          'C8FEE': new BigNumber(0),
+          'leader': copyOrder.leader,
+          'follower': copyOrder.follower,
+          'reward': 0,
+          'relayFee': 0,
+          'orderHashes': [openOrder.leader_tx_hash,
+            copyOrder.leader_tx_hash,
+            openOrder.tx_hash,
+            closeTrade.txHash],
+        });
+      }
     }
-
-    if (avg < tokenSellLastPrice) {
-      let reward = profit.div(c8LastPrice).mul(network.LEADER_REWARD_PERCENT).toFixed(0);
-      let fee = profit.div(c8LastPrice).mul(network.SYSTEM_FEE_PERCENT).toFixed(0);
-      let C8FEE = profit.div(c8LastPrice.mul(10 ** c8Decimals));
-      sumC8FEE = sumC8FEE.add(C8FEE);
-
-      processedFees.push({
-        'C8FEE': C8FEE,
-        'leader': copyOrder.leader,
-        'follower': copyOrder.follower,
-        'reward': reward,
-        'relayFee': fee,
-        'orderHashes': [openOrder.leader_tx_hash,
-          copyOrder.leader_tx_hash,
-          openOrder.tx_hash,
-          closeTrade.txHash],
-      });
-    } else {
-      processedFees.push({
-        'C8FEE': new BigNumber(0),
-        'leader': copyOrder.leader,
-        'follower': copyOrder.follower,
-        'reward': 0,
-        'relayFee': 0,
-        'orderHashes': [openOrder.leader_tx_hash,
-          copyOrder.leader_tx_hash,
-          openOrder.tx_hash,
-          closeTrade.txHash],
-      });
-    }
+  } else {
+    let reward = new BigNumber(network.REWARD);
+    let fee = new BigNumber(network.FEE);
+    let C8FEE = reward.add(fee).div(10 ** c8Decimals);
+    sumC8FEE = sumC8FEE.add(C8FEE);
+    processedFees.push({
+      'C8FEE': C8FEE,
+      'leader': copyOrder.leader,
+      'follower': copyOrder.follower,
+      'reward': network.REWARD,
+      'relayFee': network.FEE,
+      'orderHashes': ['0x',
+        copyOrder.leader_tx_hash,
+        '0x',
+        closeTrade.txHash],
+    });
   }
-  return { 'processedFees': processedFees, 'updateAmounts': updateAmounts, 'sumFee': sumC8FEE };
+  return {'processedFees': processedFees, 'updateAmounts': updateAmounts, 'sumFee': sumC8FEE};
+
 };
 
 module.exports = feeProcessor;
