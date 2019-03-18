73f1b05d36376ed420c16d5abbc42a42af061524
aueiei <chotipat@siamsquared.com>
2019-03-18 13:50:18 +0700

    Exit process if error occurred.

--- relay.js
+++ relay.js
@@ -13,33 +13,37 @@ const c8Contract = new web3.eth.Contract(
 );
 
 const follow = c8Contract.events.Follow({}, (error, event) => {
-    const redis = require("redis"), client = redis.createClient();
-    client.select(network.redis_db);
-    if (error) return console.error(error);
-    console.log('Successfully followed!', event);
+  const redis = require("redis"), client = redis.createClient();
+  client.select(network.redis_db);
+  if (error) return console.error(error);
+  console.log('Successfully followed!', event);
 
-    if (event.event === 'Follow' && event.removed === false) {
-      let leader = event.returnValues.leader.toLowerCase();
-      let follower = event.returnValues.follower.toLowerCase();
-      let percentage = event.returnValues.percentage / 10 ** 18;
-      client.hset('leader:'+leader, follower, percentage);
-    }
+  if (event.event === 'Follow' && event.removed === false) {
+    let leader = event.returnValues.leader.toLowerCase();
+    let follower = event.returnValues.follower.toLowerCase();
+    let percentage = event.returnValues.percentage / 10 ** 18;
+    client.hset('leader:' + leader, follower, percentage);
+  }
 
-  })
-  .on('error', console.error);
+}).on('error', function (error) {
+  console.log('error: ', error);
+  process.exit()
+});
 
 
-const unfollow = c8Contract.events.UnFollow({ }, (error, event) => {
-    const redis = require("redis"), client = redis.createClient();
-    client.select(network.redis_db);
-    if (error) return console.error(error, 'sad');
-    console.log('Successfully unfollowed!', event);
-    if (event.event === 'UnFollow' && event.removed === false) {
-      let leader = event.returnValues.leader.toLowerCase();
-      let follower = event.returnValues.follower.toLowerCase();
-      client.hdel('leader:'+leader, follower);
-    }
-  })
-  .on('error', console.error);
+const unfollow = c8Contract.events.UnFollow({}, (error, event) => {
+  const redis = require("redis"), client = redis.createClient();
+  client.select(network.redis_db);
+  if (error) return console.error(error, 'sad');
+  console.log('Successfully unfollowed!', event);
+  if (event.event === 'UnFollow' && event.removed === false) {
+    let leader = event.returnValues.leader.toLowerCase();
+    let follower = event.returnValues.follower.toLowerCase();
+    client.hdel('leader:' + leader, follower);
+  }
+}).on('error', function (error) {
+  console.log('error: ', error);
+  process.exit()
+});
 
 
